edf6337b85a168eb0b7e36df19973678681a49fe
Anthony Hastings <ahastings@shopkeep.com>
2017-01-27 20:30:54 +0000

    Adding port environment variable for Heroku.

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+if (typeof process.env.PORT === 'undefined') {
+  throw new Error('Server.js: PORT environment variable not defined.');
+}
+
 app.use(express.static('dist'));
-app.listen(8080);
+app.listen(process.env.PORT);
+console.info(`Listening on port ${process.env.PORT}`);
