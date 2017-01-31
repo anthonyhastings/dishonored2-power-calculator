5a0deb4f9a1083b63afea4b4009db368acd62d1e
Anthony Hastings <ahastings@shopkeep.com>
2017-01-31 22:59:54 +0000

    Adding simple total runes selector.

--- src/scripts/selectors/index.js
+++ src/scripts/selectors/index.js
@@ -1,1 +1,17 @@
 export const characterSelector = (state) => state.get('character');
+
+export const totalRunesSelector = (state) => state.get('totalRunes');
+
+// TODO: Create topLevelPowers and topLevelEnhancements selectors.
+// Read that redux "composed selectors" page for ideas on how to conjoin them.
+//
+// const topLevelPowers = selectorFunction(
+//   characterSelector,
+//   function(character) {
+//     if (_.isUndefined(character)) {
+//       return undefined;
+//     }
+//
+//    GET POWERS AND FILTER.
+//   }
+// )
