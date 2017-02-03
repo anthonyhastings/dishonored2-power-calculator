8550d28af5f1b517b8a1cb13bb137ead4149e7fc
Anthony Hastings <ahastings@shopkeep.com>
2017-02-03 13:52:08 +0000

    Adding redux-dev-tools hooks (hopefully).

--- src/scripts/flux/store.js
+++ src/scripts/flux/store.js
@@ -1,4 +1,13 @@
 import {createStore} from 'redux';
 import mainReducer from './reducers';
 
-export default createStore(mainReducer);
+const isProduction = process.env.NODE_ENV;
+let store;
+
+if (isProduction) {
+  store = createStore(mainReducer);
+} else {
+  store = createStore(mainReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+
+export default store;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,6 @@ import {Provider} from 'react-redux';
 import store from './flux/store';
 import App from './containers/app';
 
-console.info('Node Env:', process.env.NODE_ENV);
-
 ReactDOM.render((
   <Provider store={store}>
     <App />
