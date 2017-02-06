67c5f768ca0e870e199f4949a253546debeea4c2
Anthony Hastings <ahastings@shopkeep.com>
2017-02-06 16:12:32 +0000

    Adding input/simple selector tests.

--- src/scripts/flux/selectors/tests/index.spec.js
+++ src/scripts/flux/selectors/tests/index.spec.js
@@ -1,7 +1,12 @@
 import Immutable from 'immutable';
-import {characterSelector} from '../';
+import {
+  characterSelector,
+  totalRunesSelector,
+  purchasesSelector,
+  powersSelector
+} from '../';
 
-describe('selectors', function () {
+describe('input/simple selectors', function () {
   describe('characterSelector', function () {
     beforeEach(function () {
       this.state = Immutable.fromJS({
@@ -13,6 +18,55 @@ describe('selectors', function () {
 
     it('should return appropriate value', function () {
       expect(characterSelector(this.state)).toEqual('anthony');
+    });
+  });
+
+  describe('totalRunesSelector', function () {
+    beforeEach(function () {
+      this.state = Immutable.fromJS({
+        user: {
+          totalRunes: 30
+        }
+      });
+    });
+
+    it('should return appropriate value', function () {
+      expect(totalRunesSelector(this.state)).toEqual(30);
+    });
+  });
+
+  describe('purchasesSelector', function () {
+    beforeEach(function () {
+      this.state = Immutable.fromJS({
+        user: {
+          purchases: ['uuid-01', 'uuid-02']
+        }
+      });
+    });
+
+    it('should return appropriate value', function () {
+      expect(purchasesSelector(this.state)).toEqualImmutable(Immutable.fromJS([
+        'uuid-01',
+        'uuid-02'
+      ]));
+    });
+  });
+
+  describe('powersSelector', function () {
+    beforeEach(function () {
+      this.state = Immutable.fromJS({
+        powers: {
+          corvo: {},
+          emily: {}
+        }
+      });
+    });
+
+    it('should return appropriate value', function () {
+      expect(powersSelector(this.state)).toEqualImmutable(Immutable.fromJS({
+        corvo: {},
+        emily: {}
+      }));
     });
   });
 });
