b8323636ec2bab0ece8692ff5c1ed1eae08b0fd6
Anthony Hastings <ahastings@shopkeep.com>
2017-02-27 21:50:49 +0000

    Adding two new actions to add/remove purchases.

--- src/scripts/flux/actions.js
+++ src/scripts/flux/actions.js
@@ -2,3 +2,13 @@ export const setCharacter = (character) => ({
   type: 'SET_CHARACTER',
   character
 });
+
+export const addPurchase = (powerId) => ({
+  type: 'ADD_PURCHASE',
+  powerId
+});
+
+export const removePurchase = (powerId) => ({
+  type: 'REMOVE_PURCHASE',
+  powerId
+});
--- src/scripts/flux/tests/actions.spec.js
+++ src/scripts/flux/tests/actions.spec.js
@@ -16,4 +16,36 @@ describe('flux actions', function () {
       });
     });
   });
+
+  describe('addPurchase', function () {
+    describe('should create an action object', function () {
+      beforeEach(function () {
+        this.action = actions.addPurchase('power-uuid-01');
+      });
+
+      it('with the correct action type', function () {
+        expect(this.action.type).toEqual('ADD_PURCHASE');
+      });
+
+      it('with the correct character', function () {
+        expect(this.action.powerId).toEqual('power-uuid-01');
+      });
+    });
+  });
+
+  describe('removePurchase', function () {
+    describe('should create an action object', function () {
+      beforeEach(function () {
+        this.action = actions.removePurchase('power-uuid-01');
+      });
+
+      it('with the correct action type', function () {
+        expect(this.action.type).toEqual('REMOVE_PURCHASE');
+      });
+
+      it('with the correct character', function () {
+        expect(this.action.powerId).toEqual('power-uuid-01');
+      });
+    });
+  });
 });
