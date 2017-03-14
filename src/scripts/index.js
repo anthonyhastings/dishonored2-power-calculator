a2419d31b1f14e140caa6b268ac24dd8ffebced0
Anthony Hastings <ahastings@shopkeep.com>
2017-03-14 20:21:48 +0000

    Adding react-router-dom to the project.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 import store from './flux/store';
 import App from './containers/app';
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>
 ), document.querySelector('#application'));
