704d425b6aba0f337ef399d8ad0ef6e7ab21a819
Anthony Hastings <ahastings@shopkeep.com>
2017-03-21 22:19:29 +0000

    Fixing routing problem.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import store from './flux/store';
-import App from './components/app/container';
+import App from './components/app';
 
 ReactDOM.render((
   <Provider store={store}>
