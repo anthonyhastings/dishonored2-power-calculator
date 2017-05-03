7110c0f98d92f9a9aef475a5387f99c5d59d6061
Anthony Hastings <ahastings@shopkeep.com>
2017-05-03 22:55:56 +0100

    Removing unused utility.

--- src/scripts/utils/validate-uuid.js
+++
@@ -1,5 +0,0 @@
-export default function validateUUID (inputString = '') {
-  const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
-
-  return uuidRegex.test(inputString);
-}
