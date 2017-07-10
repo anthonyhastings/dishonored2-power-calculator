20dcd634638411469dad7276f30adbea6d305bc4
Anthony Hastings <ahastings@shopkeep.com>
2017-07-10 22:43:59 +0100

    Passing all GET requests on the production server to our index.html file for client-side routing.

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+const indexFilePath = path.resolve('dist/index.html');
 
 if (typeof process.env.PORT === 'undefined') {
   throw new Error('Server.js: PORT environment variable not defined.');
 }
 
 app.use(express.static('dist'));
+
+app.get('*', function (request, response) {
+  response.sendFile(indexFilePath);
+});
+
 app.listen(process.env.PORT);
 console.info(`Listening on port ${process.env.PORT}`);
