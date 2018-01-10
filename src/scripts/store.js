3fa441cd7c349c978dbb71d2080f8cb564bd4d8e
Anthony Hastings <ahastings@shopkeep.com>
2018-01-10 21:42:29 +0000

    Refactoring store setup.

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -5,20 +5,18 @@ import characters from './reducers/characters';
 import powers from './reducers/powers';
 import user from './reducers/user';
 
-let store;
-const isProduction = process.env.NODE_ENV;
+const isProduction = (process.env.NODE_ENV === 'production');
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleWare = applyMiddleware(thunk);
+
+const storeEnhancer = (isProduction) ? middleWare : composeEnhancers(middleWare);
+
 const rootReducer = combineReducers({
   characters,
   powers,
   user
 });
 
-if (isProduction) {
-  store = createStore(rootReducer, applyMiddleware(thunk));
-} else {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-}
-
-export default store;
+export default createStore(rootReducer, storeEnhancer);
