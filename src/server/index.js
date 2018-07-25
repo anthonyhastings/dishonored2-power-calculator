b2d04264e19a6dcc1c99142290c1e468db3290ef
Anthony Hastings <ar.hastings@gmail.com>
2018-07-25 21:20:01 +0100

    Logging express requests and binding to 0.0.0.0 explicitly.

--- src/server/index.js
+++ src/server/index.js
@@ -8,11 +8,17 @@ if (typeof process.env.PORT === 'undefined') {
   throw new Error('Server.js: PORT environment variable not defined.');
 }
 
+app.use(function (request, response, next) {
+  console.log(`Express - Request: ${request.url}`);
+  next();
+});
+
 app.use(express.static('dist'));
 
 app.get('*', function (request, response) {
   response.sendFile(indexFilePath);
 });
 
-app.listen(process.env.PORT);
-console.info(`Listening on port ${process.env.PORT}`);
+app.listen(process.env.PORT, '0.0.0.0', () => {
+  console.info(`Listening on port 0.0.0.0:${process.env.PORT}`);
+});
