37408f26c381770424cdd7abaa9578de7a643f8a
Anthony Hastings <ar.hastings@gmail.com>
2019-07-30 22:59:20 +0100

    Moving test support to top-level file.

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,12 @@ module.exports = {
   coverageDirectory: './jest-coverage',
   coverageReporters: ['lcov', 'text-summary'],
   moduleNameMapper: {
-    '\\.(css|scss|sass)$': '<rootDir>/src/test-support/style-mock.js'
+    '\\.(css|scss|sass)$': '<rootDir>/test-support/style-mock.js'
   },
   transform: {
     '^.+\\.js$': 'babel-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/test-support/file-transformer.js'
+      '<rootDir>/test-support/file-transformer.js'
   },
   verbose: true
 };
