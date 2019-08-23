ad0e7f9865ec5718274105d2a5dabc307dcc4dcf
Anthony Hastings <ar.hastings@gmail.com>
2019-08-23 21:20:54 +0100

    Implementing react-axe into development builds.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* global process */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -5,13 +7,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import App from './components/app/container';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Router>
-  </Provider>,
-  document.querySelector('#application')
-);
+const renderApplication = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Router>
+    </Provider>,
+    document.querySelector('#application')
+  );
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  import('react-axe').then(({ default: axe }) => {
+    axe(React, ReactDOM, 1000);
+    renderApplication();
+  });
+} else {
+  renderApplication();
+}
