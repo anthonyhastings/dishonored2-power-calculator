da459aac4d60619031072f6b90f42870e3429cb0
Anthony Hastings <ar.hastings@gmail.com>
2019-11-06 21:33:04 +0000

    Making totalRunes a constant rather then flux state. (#44)

--- src/reducers/user/selectors/tests/remaining-runes.spec.js
+++ src/reducers/user/selectors/tests/remaining-runes.spec.js
@@ -1,60 +1,9 @@
-import Immutable from 'immutable';
 import remainingRunesSelector from '../remaining-runes';
 
-describe('#remainingRunesTransform', () => {
-  let state;
+jest.mock('Constants/total-runes', () => 10);
 
-  const powersAndEnhancements = Immutable.fromJS({
-    'uuid-01': {
-      name: 'Power #01',
-      cost: 10
-    },
-    'uuid-02': {
-      name: 'Power #02',
-      cost: 15
-    },
-    'uuid-03': {
-      name: 'Power #03',
-      cost: 10
-    }
-  });
-
-  beforeEach(() => {
-    remainingRunesSelector.resetRecomputations();
-
-    state = Immutable.fromJS({
-      powers: {
-        data: powersAndEnhancements
-      },
-      user: {
-        purchases: ['uuid-01', 'uuid-02'],
-        totalRunes: 30
-      }
-    });
-  });
-
-  it('should return the remainder', () => {
-    expect(remainingRunesSelector(state)).toEqual(5);
-  });
-
-  describe('when called multiple times with same input', () => {
-    beforeEach(() => {
-      state = Immutable.fromJS({
-        powers: {
-          data: powersAndEnhancements
-        },
-        user: {
-          purchases: ['uuid-03'],
-          totalRunes: 30
-        }
-      });
-
-      remainingRunesSelector(state);
-      remainingRunesSelector(state);
-    });
-
-    it('is memoized and only called once', () => {
-      expect(remainingRunesSelector.recomputations()).toEqual(1);
-    });
+describe('#remainingRunesSelector', () => {
+  it('returns the remainder of deducting spent runes from total runes', () => {
+    expect(remainingRunesSelector.resultFunc(8)).toEqual(2);
   });
 });
