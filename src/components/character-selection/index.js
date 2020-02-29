5040b2ea97657089ada0241ace5dd27fc1e968f7
Anthony Hastings <ar.hastings@gmail.com>
2020-02-29 23:30:01 +0000

    Updating CharacterSelection component and styles. (#55)

--- src/components/character-selection/index.js
+++ src/components/character-selection/index.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Link } from 'react-router-dom';
+import corvoPortrait from './images/corvo.jpg';
+import emilyPortrait from './images/emily.jpg';
+import './stylesheets/index.scss';
 
-const CharacterSelection = (props) => {
-  const characterElements = props.characters.valueSeq().map((character) => (
-    <div className="character__container" key={character.get('id')}>
-      <h2 className="character__name">{character.get('name')}</h2>
-      <p className="character__description">{character.get('description')}</p>
-      <Link to={`/${character.get('slug')}/powers`}>
-        Choose {character.get('name')}
-      </Link>
-    </div>
-  ));
+const characterNamespace = 'character';
 
+const slugsToPortraits = new Map([
+  ['corvo', corvoPortrait],
+  ['emily', emilyPortrait]
+]);
+
+const CharacterSelection = ({ characters }) => {
   return (
-    <section className="characterSelection__container">
-      <h1>Character Selection</h1>
-      {characterElements}
+    <section className="character-selection">
+      <h1 className="character-selection__title">Choose your character</h1>
+      <div className="character-selection__grid">
+        {characters.valueSeq().map((character) => (
+          <div
+            className={`character-selection__grid-element ${characterNamespace}`}
+            key={character.get('id')}
+          >
+            <img
+              className={`${characterNamespace}__portrait`}
+              draggable="false"
+              src={slugsToPortraits.get(character.get('slug'))}
+              alt={`Portrait of ${character.get('name')}`}
+            />
+            <div className={`${characterNamespace}__overlay`}>
+              <h2 className={`${characterNamespace}__name`}>
+                {character.get('name')}
+              </h2>
+              <p className={`${characterNamespace}__description`}>
+                {character.get('description')}
+              </p>
+              <Link
+                className={`${characterNamespace}__button`}
+                to={`/${character.get('slug')}/powers`}
+              >
+                Choose {character.get('name')}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
