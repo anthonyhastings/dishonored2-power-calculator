33f38444f7766b41e7a12fffffeabbb0380281d3
Anthony Hastings <ar.hastings@gmail.com>
2020-05-10 22:32:54 +0100

    Adding CopyWebpackPlugin declaration to move manifest and assets.

--- support/paths.js
+++ support/paths.js
@@ -8,6 +8,7 @@ module.exports = {
   constants: path.join(projectRoot, 'src/constants'),
   dist: path.join(projectRoot, 'dist'),
   images: path.join(projectRoot, 'images'),
+  manifests: path.join(projectRoot, 'manifests'),
   reducers: path.join(projectRoot, 'src/reducers'),
   server: path.join(projectRoot, 'server'),
   src: path.join(projectRoot, 'src')
