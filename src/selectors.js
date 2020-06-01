ffa392a3469c67246004507914add09a1130a925
Anthony Hastings <ar.hastings@gmail.com>
2020-06-01 23:06:22 +0100

    Fixing and DRYing selectors and their tests.

--- src/selectors.js
+++ src/selectors.js
@@ -10,28 +10,31 @@ const isRequestIncomplete = (requestStatus) =>
 const hasRequestFailed = (requestStatus) =>
   requestStatus === requestStatuses.failure;
 
+const filterTopLeverPowersList = (records) =>
+  records.filter((record) => record.get('parentPowerId') === null).toList();
+
 const charactersSelector = (state) => state.get('characters', Immutable.Map());
+
+const powersSelector = (state) => state.get('powers', Immutable.Map());
 
 export const charactersDataSelector = createSelector(
   charactersSelector,
   charactersReducerSelectors.dataSelector
 );
 
-export const characterBySlugSelector = (state, slug) => {
-  const characters = charactersDataSelector(state);
-  return characters.find((character) => character.get('slug') === slug);
-};
-
 const charactersRequestStatusSelector = createSelector(
   charactersSelector,
   charactersReducerSelectors.requestStatusSelector
 );
 
-const powersSelector = (state) => state.get('powers', Immutable.Map());
-
 const powersRequestStatusSelector = createSelector(
   powersSelector,
   powersReducerSelectors.requestStatusSelector
+);
+
+const powersDataSelector = createSelector(
+  powersSelector,
+  powersReducerSelectors.dataSelector
 );
 
 export const isInitialDataIncompleteSelector = createSelector(
@@ -45,3 +48,38 @@ export const hasInitialDataFailedSelector = createSelector(
   powersRequestStatusSelector,
   (...statuses) => statuses.some(hasRequestFailed)
 );
+
+export const characterBySlugSelector = (state, slug) => {
+  const characters = charactersDataSelector(state);
+  return characters.find((character) => character.get('slug') === slug);
+};
+
+const enhancementRecordsSelector = createSelector(powersDataSelector, (data) =>
+  data.filter((record) => record.get('type') === 'enhancement')
+);
+
+const powerRecordsSelector = createSelector(powersDataSelector, (data) =>
+  data.filter((record) => record.get('type') === 'power')
+);
+
+export const topLevelEnhancementsSelector = createSelector(
+  enhancementRecordsSelector,
+  filterTopLeverPowersList
+);
+
+const topLevelPowersSelector = createSelector(
+  powerRecordsSelector,
+  filterTopLeverPowersList
+);
+
+export const topLevelPowersByCharacterSlugSelector = (state, characterSlug) => {
+  const character = characterBySlugSelector(state, characterSlug);
+
+  return topLevelPowersSelector(state)
+    .filter((power) => {
+      const belongsTo = power.get('characterId') === character.get('id');
+      const belongsToAll = power.get('characterId') === null;
+      return belongsTo || belongsToAll;
+    })
+    .toList();
+};
