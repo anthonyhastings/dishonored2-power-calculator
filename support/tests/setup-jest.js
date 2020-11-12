950b34938fedac4b27cc21e99e0cc3966cdf35c4
Anthony Hastings <ar.hastings@gmail.com>
2020-11-12 21:57:25 +0000

    Adding custom serializer to stop double escaping by Jest.

--- support/tests/setup-jest.js
+++ support/tests/setup-jest.js
@@ -1,3 +1,4 @@
-import { toMatchDiffSnapshot } from 'snapshot-diff';
+import snapshotDiff, { toMatchDiffSnapshot } from 'snapshot-diff';
 
+expect.addSnapshotSerializer(snapshotDiff.getSnapshotDiffSerializer());
 expect.extend({ toMatchDiffSnapshot });
