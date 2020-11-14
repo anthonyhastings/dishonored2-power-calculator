10d17b68d323a302f46b9bce2ed77d7ec9ac7445
Anthony Hastings <ar.hastings@gmail.com>
2020-11-14 22:51:31 +0000

    Removing redux-mock-store package. (#92)

--- src/reducers/characters/tests/index.spec.js
+++ src/reducers/characters/tests/index.spec.js
@@ -1,8 +1,6 @@
 import Immutable from 'immutable';
-import configureMockStore from 'redux-mock-store';
 import getCharactersSuccessResponse from 'Api/sample-responses/get-characters-success';
 import requestStatuses from 'Constants/request-statuses';
-import { middleware } from '../../../store';
 import { getCharacters as getCharactersMock } from 'Api/characters';
 import reducer, {
   GET_CHARACTERS_PENDING,
@@ -87,70 +85,56 @@ describe('Characters reducer', () => {
 
   describe('action creators', () => {
     describe('#getCharacters', () => {
-      beforeEach(() => {
-        testContext.mockStore = configureMockStore(middleware)();
-      });
-
-      describe('when the request triggers', () => {
-        beforeEach(async () => {
-          getCharactersMock.mockResolvedValue();
-          await testContext.mockStore.dispatch(getCharacters());
-        });
-
-        it('fires a requested action', () => {
-          expect(testContext.mockStore.getActions()[0]).toEqual({
-            type: GET_CHARACTERS_PENDING,
-          });
-        });
-      });
-
       describe('when the request succeeds', () => {
         beforeEach(async () => {
+          testContext.mockDispatch = jest.fn();
           getCharactersMock.mockResolvedValue({
             data: getCharactersSuccessResponse,
           });
 
-          await testContext.mockStore.dispatch(getCharacters());
+          await getCharacters()(testContext.mockDispatch);
         });
 
         it('fires a requested action and a success action', () => {
-          expect(testContext.mockStore.getActions()).toEqual([
-            { type: GET_CHARACTERS_PENDING },
-            {
-              type: GET_CHARACTERS_SUCCESS,
-              payload: {
-                characters: Immutable.fromJS({
-                  'fake-uuid-01': {
-                    id: 'fake-uuid-01',
-                    slug: 'fake-slug-01',
-                    name: 'fake-name-01',
-                    description: 'fake-description-01',
-                  },
-                  'fake-uuid-02': {
-                    id: 'fake-uuid-02',
-                    slug: 'fake-slug-02',
-                    name: 'fake-name-02',
-                    description: 'fake-description-02',
-                  },
-                }),
+          expect(testContext.mockDispatch.mock.calls).toEqual([
+            [{ type: GET_CHARACTERS_PENDING }],
+            [
+              {
+                type: GET_CHARACTERS_SUCCESS,
+                payload: {
+                  characters: Immutable.fromJS({
+                    'fake-uuid-01': {
+                      id: 'fake-uuid-01',
+                      slug: 'fake-slug-01',
+                      name: 'fake-name-01',
+                      description: 'fake-description-01',
+                    },
+                    'fake-uuid-02': {
+                      id: 'fake-uuid-02',
+                      slug: 'fake-slug-02',
+                      name: 'fake-name-02',
+                      description: 'fake-description-02',
+                    },
+                  }),
+                },
               },
-            },
+            ],
           ]);
         });
       });
 
       describe('when the request fails', () => {
         beforeEach(async () => {
+          testContext.mockDispatch = jest.fn();
           getCharactersMock.mockRejectedValue();
-          await testContext.mockStore.dispatch(getCharacters());
+
+          await getCharacters()(testContext.mockDispatch);
         });
 
         it('fires a requested action and a failure action', () => {
-          expect(testContext.mockStore.getActions()).toEqual([
-            { type: GET_CHARACTERS_PENDING },
-            {
-              type: GET_CHARACTERS_FAILURE,
-            },
+          expect(testContext.mockDispatch.mock.calls).toEqual([
+            [{ type: GET_CHARACTERS_PENDING }],
+            [{ type: GET_CHARACTERS_FAILURE }],
           ]);
         });
       });
--- src/reducers/powers/tests/index.spec.js
+++ src/reducers/powers/tests/index.spec.js
@@ -1,8 +1,6 @@
 import Immutable from 'immutable';
-import configureMockStore from 'redux-mock-store';
 import getPowersSuccessResponse from 'Api/sample-responses/get-powers-success';
 import requestStatuses from 'Constants/request-statuses';
-import { middleware } from '../../../store';
 import { getPowers as getPowersMock } from 'Api/powers';
 import reducer, {
   GET_POWERS_PENDING,
@@ -87,75 +85,62 @@ describe('Powers reducer', () => {
 
   describe('action creators', () => {
     describe('#getPowers', () => {
-      beforeEach(() => {
-        testContext.mockStore = configureMockStore(middleware)();
-      });
-
-      describe('when the request triggers', () => {
-        beforeEach(async () => {
-          getPowersMock.mockResolvedValue();
-          await testContext.mockStore.dispatch(getPowers());
-        });
-
-        it('fires a requested action', () => {
-          expect(testContext.mockStore.getActions()[0]).toEqual({
-            type: GET_POWERS_PENDING,
-          });
-        });
-      });
-
       describe('when the request succeeds', () => {
         beforeEach(async () => {
+          testContext.mockDispatch = jest.fn();
           getPowersMock.mockResolvedValue({
             data: getPowersSuccessResponse,
           });
-          await testContext.mockStore.dispatch(getPowers());
+
+          await getPowers()(testContext.mockDispatch);
         });
 
         it('fires a requested action and a success action', () => {
-          expect(testContext.mockStore.getActions()).toEqual([
-            { type: GET_POWERS_PENDING },
-            {
-              type: GET_POWERS_SUCCESS,
-              payload: {
-                powers: Immutable.fromJS({
-                  'fake-uuid-01': {
-                    id: 'fake-uuid-01',
-                    parentPowerId: 'fake-parent-power-id-01',
-                    characterId: 'fake-character-id-01',
-                    type: 'fake-type-01',
-                    name: 'fake-name-01',
-                    description: 'fake-description-01',
-                    cost: 1,
-                  },
-                  'fake-uuid-02': {
-                    id: 'fake-uuid-02',
-                    parentPowerId: 'fake-parent-power-id-02',
-                    characterId: 'fake-character-id-02',
-                    type: 'fake-type-02',
-                    name: 'fake-name-02',
-                    description: 'fake-description-02',
-                    cost: 2,
-                  },
-                }),
+          expect(testContext.mockDispatch.mock.calls).toEqual([
+            [{ type: GET_POWERS_PENDING }],
+            [
+              {
+                type: GET_POWERS_SUCCESS,
+                payload: {
+                  powers: Immutable.fromJS({
+                    'fake-uuid-01': {
+                      id: 'fake-uuid-01',
+                      parentPowerId: 'fake-parent-power-id-01',
+                      characterId: 'fake-character-id-01',
+                      type: 'fake-type-01',
+                      name: 'fake-name-01',
+                      description: 'fake-description-01',
+                      cost: 1,
+                    },
+                    'fake-uuid-02': {
+                      id: 'fake-uuid-02',
+                      parentPowerId: 'fake-parent-power-id-02',
+                      characterId: 'fake-character-id-02',
+                      type: 'fake-type-02',
+                      name: 'fake-name-02',
+                      description: 'fake-description-02',
+                      cost: 2,
+                    },
+                  }),
+                },
               },
-            },
+            ],
           ]);
         });
       });
 
       describe('when the request fails', () => {
         beforeEach(async () => {
+          testContext.mockDispatch = jest.fn();
           getPowersMock.mockRejectedValue();
-          await testContext.mockStore.dispatch(getPowers());
+
+          await getPowers()(testContext.mockDispatch);
         });
 
         it('fires a requested action and a failure action', () => {
-          expect(testContext.mockStore.getActions()).toEqual([
-            { type: GET_POWERS_PENDING },
-            {
-              type: GET_POWERS_FAILURE,
-            },
+          expect(testContext.mockDispatch.mock.calls).toEqual([
+            [{ type: GET_POWERS_PENDING }],
+            [{ type: GET_POWERS_FAILURE }],
           ]);
         });
       });
